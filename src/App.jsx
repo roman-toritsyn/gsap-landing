c2ed5599ca2510595952159022b589df03c1e804
Roman Toritsyn <roman.toritsyn@otakoyi.com>
2025-01-31 17:06:29 +0200

    replased smooth-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,80 @@
 import './App.css'
 import { gsap } from "gsap";
-import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ScrollSmoother from 'gsap-trial/ScrollSmoother';
+import { useEffect } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(ScrollSmoother);
 
 function App() {
-  useGSAP(() => {
-    if (!ScrollTrigger.isTouch) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "/ScrollSmoother.min.js";
+    script.onload = () => {
+      if (window.ScrollSmoother) {
+        gsap.registerPlugin(window.ScrollSmoother);
 
-      ScrollSmoother.create({
-        wrapper: '.wrapper',
-        content: '.content',
-        smooth: 1.5,
-        effects: true
-      });
+        if (!ScrollTrigger.isTouch) {
+          window.ScrollSmoother.create({
+            wrapper: '.wrapper',
+            content: '.content',
+            smooth: 1.5,
+            effects: true
+          });
 
-      gsap.fromTo('.hero-section',
-        { opacity: 10 },
-        {
-          opacity: 0,
-          scrollTrigger: {
-            trigger: '.hero-section',
-            start: 'top center',
-            end: 'bottom top',
-            scrub: true
-          }
+          gsap.fromTo('.hero-section',
+            { opacity: 10 },
+            {
+              opacity: 0,
+              scrollTrigger: {
+                trigger: '.hero-section',
+                start: 'top center',
+                end: 'bottom top',
+                scrub: true
+              }
+            }
+          );
+
+          let itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
+          itemsL.forEach(item => {
+            gsap.fromTo(item,
+              { opacity: 0, x: -50 },
+              {
+                opacity: 1, x: 0,
+                scrollTrigger: {
+                  trigger: item,
+                  start: 'top 80%',
+                  end: 'top 20%',
+                  scrub: true
+                }
+              }
+            );
+          });
+
+          let itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
+          itemsR.forEach(item => {
+            gsap.fromTo(item,
+              { opacity: 0, x: 400 },
+              {
+                opacity: 1, x: 0,
+                scrollTrigger: {
+                  trigger: item,
+                  start: 'top 80%',
+                  end: 'top 20%',
+                  scrub: true
+                }
+              }
+            );
+          });
         }
-      );
+      } else {
+        console.error("ScrollSmoother not loaded");
+      }
+    };
+    document.body.appendChild(script);
 
-      let itemsL = gsap.utils.toArray('.gallery__left .gallery__item');
-      itemsL.forEach(item => {
-        gsap.fromTo(item,
-          { opacity: 0, x: -50 },
-          {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-              trigger: item,
-              start: 'top 80%',
-              end: 'top 20%',
-              scrub: true
-            }
-          }
-        );
-      });
-
-      let itemsR = gsap.utils.toArray('.gallery__right .gallery__item');
-      itemsR.forEach(item => {
-        gsap.fromTo(item,
-          { opacity: 0, x: 400 },
-          {
-            opacity: 1, x: 0,
-            scrollTrigger: {
-              trigger: item,
-              start: 'top 80%',
-              end: 'top 20%',
-              scrub: true
-            }
-          }
-        );
-      });
-    }
+    return () => {
+      document.body.removeChild(script);
+    };
   }, []);
 
   return (
